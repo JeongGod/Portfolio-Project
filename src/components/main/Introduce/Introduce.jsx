9c170c1a04957c8266d9d71e1d3632e88e035aca
이정규 <jeonggu223@gmail.com>
2021-08-16 18:31:45 +0900

    feat
    1. MainPage 폴더 구조 정리
    2. MainPage template 정리
    3. Education Component 추가 및 수정 기능
    4. Awards, Projects, Certifications 수정 기능

---
+++ src/components/main/Introduce/Introduce.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Education from "./Education";
+import Awards from "./Awards";
+import Projects from "./Projects";
+import Certifications from "./Certifications";
+
+const Introduce = () => {
+  return (
+    <>
+      <Education />
+      <Awards />
+      <Projects />
+      <Certifications />
+    </>
+  );
+};
+
+export default Introduce;
