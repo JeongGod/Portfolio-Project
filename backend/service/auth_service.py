19cd903ababa06baf57794a555889ea3792851ca
JeongGod <jeonggu223@gmail.com>
2021-08-18 15:05:20 +0900

    feat : 회원가입시 user정보 확인

--- backend/service/auth_service.py
+++ backend/service/auth_service.py
@@ -5,6 +5,13 @@ class Auth:
 
     def signup_user(data):
         new_racer = racers(data.racer_id, data.racer_pw, data.racer_name, data.image, data.introduce)
-        db.session.add(new_racer)
-        db.session.commit()
-        return data.racer_id
+        user = db.session.query(racers).filter(racers.racer_id == new_racer.racer_id)
+        if user is None:
+            try:
+                db.session.add(new_racer)
+                db.session.commit()
+                return "success"
+            except Exception.SQLALchemyError:
+                db.session.rollback()
+                return "fail"
+        return "exist"
