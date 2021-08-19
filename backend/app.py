1087339b49f959909db5253614e6b23460bfa0aa
JeongGod <jeonggu223@gmail.com>
2021-08-19 23:57:01 +0900

    flask-restful에서 blueprint로 변경

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,6 @@
-from controller.user_controller import PortfolioUser
-from controller.auth_controller import Login, Logout, SignUp
+from controller.user_controller import user
+from controller.auth_controller import auth
 from flask import Flask
-from flask_restful import Api
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
@@ -16,11 +15,8 @@ def create_app():
     jwt = JWTManager(app)
 
     # API 설정
-    api = Api(app)
-    # 인증 API
-    api.add_resource(Login, '/login')
-    api.add_resource(Logout, '/logout')
-    api.add_resource(SignUp, '/signup')
+    app.register_blueprint(auth)
+    app.register_blueprint(user)
 
     # DB 및 마이그레이션 설정
     app.config.from_object(config)
--- backend/service/auth_service.py
+++ backend/service/auth_service.py
@@ -6,26 +6,36 @@ from flask_jwt_extended import create_access_token, create_refresh_token
 
 class Auth:
     def login_user(data):
-        user = db.session.query(racers).filter(
-            racers.racer_id == data.id).first()
+        try :
+            user = db.session.query(racers).filter(
+                racers.racer_id == data.get('id')).first()
 
-        if user is None:
-            return jsonify(result="no exist")
-        elif not checkpw(data.pw.encode('utf-8'), user.racer_pw.encode('utf-8')):
-            return jsonify(result="not equal")
-        else:
-            # 로그인 성공 => JWT토큰 발행
-            access_token = create_access_token(identity = user.racer_id)
-            refresh_token = create_refresh_token(identity= user.racer_id)
-            user.setToken(refresh_token)
-            return jsonify(result="success", access_token=access_token, refresh_token=refresh_token)
+            if user is None:
+                return jsonify(result="no exist")
+            elif not checkpw(data.get('pw').encode('utf-8'), user.racer_pw.encode('utf-8')):
+                return jsonify(result="not equal")
+            else:
+                # 로그인 성공 => JWT토큰 발행
+                access_token = create_access_token(identity = user.racer_id)
+                refresh_token = create_refresh_token(identity= user.racer_id)
+                user.setToken(refresh_token)
+                return jsonify(result="success", access_token=access_token, refresh_token=refresh_token)
+        except Exception as e:
+            print(e)
+            return jsonify(result="fail")
+
+    def logout_user(data):
+        user = db.session.query(racers).filter(racers.token == data).first()
+        if user:
+            user.setToken(None)
+        return jsonify(result="success")
 
     def signup_user(data):
         user = db.session.query(racers).filter(
-            racers.racer_id == data.id).first()
+            racers.racer_id == data.get('id')).first()
 
         if user is None:
-            new_racer = racers(data.id, data.pw, data.name)
+            new_racer = racers(data.get('id'), data.get('pw'), data.get('name'))
             try:
                 db.session.add(new_racer)
                 db.session.commit()
@@ -35,8 +45,4 @@ class Auth:
                 return jsonify(result="fail")
         return jsonify(result="exist")
     
-    def logout_user(data):
-        user = db.session.query(racers).filter(racers.token == data).first()
-        if user:
-            user.setToken(None)
-        return jsonify(result="success")
+    
