5b4c6bc0ed3b892718f2885255f355cc47e263c6
JeongGod <jeonggu223@gmail.com>
2021-08-26 17:09:19 +0900

    feat : google login client와 연동

--- backend/controller/google_auth.py
+++ backend/controller/google_auth.py
@@ -13,20 +13,18 @@ REDIRECT_URI = 'http://localhost:5000/auth/google/authorize'
 def login():
     return redirect(new_url)
 
-@google_auth.route('/authorize')
+@google_auth.route('/authorize', methods=['POST'])
 def authorize():
-    if 'code' not in request.args:
+    if 'code' not in request.json:
         auth_uri = ('https://accounts.google.com/o/oauth2/v2/auth?response_type=code'
                     '&client_id={}&redirect_uri={}&scope={}').format(GOOGLE_OAUTH2_CLIENT_ID, REDIRECT_URI, SCOPE)
         return redirect(auth_uri)
     else:
-        auth_code = request.args.get('code')
+        auth_code = request.json.get('code')
         data = {'code': auth_code,
                 'client_id': GOOGLE_OAUTH2_CLIENT_ID,
                 'client_secret': GOOGLE_OAUTH2_CLIENT_SECRET,
-                'redirect_uri': REDIRECT_URI,
+                'redirect_uri': "http://localhost:3000",
                 'grant_type': 'authorization_code'}
-        tokens = requests.post('https://oauth2.googleapis.com/token', data=data)
-
+        tokens = requests.post("https://oauth2.googleapis.com/token", data=data)
         return Google_Auth.login_user(tokens.json())
-
--- backend/service/google_auth_service.py
+++ backend/service/google_auth_service.py
@@ -1,29 +1,35 @@
 import requests
 from db_connect import db
-from flask import jsonify
+from flask import jsonify, redirect
+from flask_jwt_extended import create_access_token, create_refresh_token
 from models.racers import racers
 
 
 class Google_Auth:
     def login_user(tokens):
         # google에서 발급한 token
-        access_token = tokens.get('access_token')
-        refresh_token = tokens.get('refresh_token')
+        google_access_token = tokens.get('access_token')
+        google_refresh_token = tokens.get('refresh_token')
+
         # 유저 정보 받아오기
         headers = {
-            'Authorization' : f'Bearer {access_token}'
+            'Authorization' : f'Bearer {google_access_token}'
         }
         profile = requests.get('https://www.googleapis.com/oauth2/v1/userinfo', headers=headers).json()
-        
+
         try :
-            racer = db.session.query(racers).filter(racers.racer_id == profile.email).first()
+            email = profile.get('email')
+            name = profile.get('name')
+            user = db.session.query(racers).filter(racers.racer_id == email).first()
             # 유저가 없다면 회원가입
-            if racer is None:
-                # 비밀번호 어떻게 처리할 지 생각 => 일단은 access_token으로 처리
-                new_racer = racers(profile.email, access_token, profile.name)
-                new_racer.setToken(tokens.get('refresh_token'))
-                db.session.add(new_racer)
+            if user is None:
+                # 비밀번호 어떻게 처리할 지 생각 => 일단은 google_access_token으로 처리
+                user = racers(email, google_access_token, name)
+                db.session.add(user)
                 db.session.commit()
+            access_token = create_access_token(identity = user.racer_id)
+            refresh_token = create_refresh_token(identity= user.racer_id)
+            user.setToken(refresh_token)
             return jsonify(result="success", access_token=access_token, refresh_token=refresh_token)
         
         except Exception as e:
